84cadf7355d021a6fc26d14fdba31643c0297bbb
MSI\moneo <jvr.moneo@gmail.com>
2020-10-02 13:23:12 +0200

    Añadir pokemons para seleccionar

--- js/pokecombat.js
+++ js/pokecombat.js
@@ -1,5 +1,38 @@
 
-
+// SELECT FIGTHER
+function selectFigther1(){
+    const element = document.getElementById('id-img-fighter-left');
+    element.src= 'img/pokemons/icons8-bullbasaur-96.png';
+}
+function selectFigther2(){
+    console.log('cambio icono 2')
+    const element = document.getElementById('id-img-fighter-left');
+    element.src= 'img/pokemons/icons8-charmander-96.png';
+}
+function selectFigther3(){
+    const element = document.getElementById('id-img-fighter-left');
+    element.src= 'img/pokemons/icons8-dratini-96.png';
+}
+function selectFigther4(){
+    const element = document.getElementById('id-img-fighter-left');
+    element.src= 'img/pokemons/icons8-eevee-96.png';
+}
+function selectFigther5(){
+    const element = document.getElementById('id-img-fighter-right');
+    element.src= 'img/pokemons/icons8-jigglypuff-96.png';
+}
+function selectFigther6(){
+    const element = document.getElementById('id-img-fighter-right');
+    element.src= 'img/pokemons/icons8-mankey-96.png';
+}
+function selectFigther7(){
+    const element = document.getElementById('id-img-fighter-right');
+    element.src= 'img/pokemons/icons8-meowth-96.png';
+}
+function selectFigther8(){
+    const element = document.getElementById('id-img-fighter-right');
+    element.src= 'img/pokemons/icons8-pikachu-pokemon-96.png';
+}
 
 // SELECT BACKGROUND
 function selectBackground1(){
