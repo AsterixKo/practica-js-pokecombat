34c7f41aff3b9b07861a132a8559f1de184d6c24
MSI\moneo <jvr.moneo@gmail.com>
2020-10-02 19:47:23 +0200

    Añadir nombre al pokemon luchador

--- js/pokecombat.js
+++ js/pokecombat.js
@@ -15,23 +15,23 @@ function readInputs() {
     lifeFigtherRight = document.getElementById('id-life-figther-right').value;
     damageFigtherRight = document.getElementById('id-damage-figther-right').value;
     defenseFigtherRight = document.getElementById('id-defense-figther-right').value;
-    console.log('-------LEFT------');
-    console.log('lifeFigtherLeft:', lifeFigtherLeft);
-    console.log('damageFigtherLeft:', damageFigtherLeft);
-    console.log('defenseFigtherLeft:', defenseFigtherLeft);
-    console.log('-------RIGHT------');
-    console.log('lifeFigtherRight:', lifeFigtherRight);
-    console.log('damageFigtherRight:', damageFigtherRight);
-    console.log('defenseFigtherRight:', defenseFigtherRight);
+    // console.log('-------LEFT------');
+    // console.log('lifeFigtherLeft:', lifeFigtherLeft);
+    // console.log('damageFigtherLeft:', damageFigtherLeft);
+    // console.log('defenseFigtherLeft:', defenseFigtherLeft);
+    // console.log('-------RIGHT------');
+    // console.log('lifeFigtherRight:', lifeFigtherRight);
+    // console.log('damageFigtherRight:', damageFigtherRight);
+    // console.log('defenseFigtherRight:', defenseFigtherRight);
 }
 
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
-function writePanelInfo(text){
+function writePanelInfo(text) {
     const panelInfo = document.getElementById('id-panel-info-fight');
-    var node = document.createElement('P'); 
+    var node = document.createElement('P');
     var textnode = document.createTextNode(text);
     node.appendChild(textnode);
     panelInfo.appendChild(node);
@@ -45,14 +45,13 @@ function figth() {
             defenseFigtherRight = defenseFigtherRight - damageFigtherLeft;
             // Mostramos información en la pagina
             writePanelInfo(`Defensa right: ${defenseFigtherRight}`);
-            console.log('defenseFigtherRight:',defenseFigtherRight);
+            console.log('defenseFigtherRight:', defenseFigtherRight);
         } else { //restamos a la vida
             lifeFigtherRight = lifeFigtherRight - damageFigtherLeft;
             // Mostramos información en la pagina
             writePanelInfo(`Vida right: ${lifeFigtherRight}`);
-            console.log('lifeFigtherRight:',lifeFigtherRight);
+            console.log('lifeFigtherRight:', lifeFigtherRight);
         }
-
 
     } else if (whoAttack === 2) { //ataca jugador derecho
         whoAttack = 1;
@@ -61,12 +60,12 @@ function figth() {
             defenseFigtherLeft = defenseFigtherLeft - damageFigtherRight;
             // Mostramos información en la pagina
             writePanelInfo(`Defensa left: ${defenseFigtherLeft}`);
-            console.log('defenseFigtherLeft:',defenseFigtherLeft);
+            console.log('defenseFigtherLeft:', defenseFigtherLeft);
         } else { //restamos a la vida
             lifeFigtherLeft = lifeFigtherLeft - damageFigtherRight;
             // Mostramos información en la pagina
             writePanelInfo(`Vida left: ${lifeFigtherLeft}`);
-            console.log('lifeFigtherLeft:',lifeFigtherLeft);
+            console.log('lifeFigtherLeft:', lifeFigtherLeft);
         }
     }
 }
@@ -81,13 +80,13 @@ function actionButton() {
     //     setTimeout(figth, 500);
 
     // }
-    var theInterval = setInterval(function (){
+    var theInterval = setInterval(function () {
         figth();
-        if(lifeFigtherLeft <= 0 || lifeFigtherRight <= 0){
-            if(lifeFigtherLeft <= 0){
+        if (lifeFigtherLeft <= 0 || lifeFigtherRight <= 0) {
+            if (lifeFigtherLeft <= 0) {
                 writePanelInfo('Gana right');
             }
-            if(lifeFigtherRight <= 0){
+            if (lifeFigtherRight <= 0) {
                 writePanelInfo('Gana left');
             }
             clearInterval(theInterval);
@@ -100,42 +99,50 @@ function actionButton() {
 function selectFigther1() {
     const element = document.getElementById('id-img-fighter-left');
     element.src = 'img/pokemons/icons8-bullbasaur-96.png';
+    document.getElementById('id-figther-left').innerText = 'Bullbasaur';
 }
 
 function selectFigther2() {
     console.log('cambio icono 2')
     const element = document.getElementById('id-img-fighter-left');
     element.src = 'img/pokemons/icons8-charmander-96.png';
+    document.getElementById('id-figther-left').innerText = 'Charmande';
 }
 
 function selectFigther3() {
     const element = document.getElementById('id-img-fighter-left');
     element.src = 'img/pokemons/icons8-dratini-96.png';
+    document.getElementById('id-figther-left').innerText = 'Dratini';
 }
 
 function selectFigther4() {
     const element = document.getElementById('id-img-fighter-left');
     element.src = 'img/pokemons/icons8-eevee-96.png';
+    document.getElementById('id-figther-left').innerText = 'Eevee';
 }
 
 function selectFigther5() {
     const element = document.getElementById('id-img-fighter-right');
     element.src = 'img/pokemons/icons8-jigglypuff-96.png';
+    document.getElementById('id-figther-right').innerText = 'Jigglypuff';
 }
 
 function selectFigther6() {
     const element = document.getElementById('id-img-fighter-right');
     element.src = 'img/pokemons/icons8-mankey-96.png';
+    document.getElementById('id-figther-right').innerText = 'Mankey';
 }
 
 function selectFigther7() {
     const element = document.getElementById('id-img-fighter-right');
     element.src = 'img/pokemons/icons8-meowth-96.png';
+    document.getElementById('id-figther-right').innerText = 'Meowth';
 }
 
 function selectFigther8() {
     const element = document.getElementById('id-img-fighter-right');
     element.src = 'img/pokemons/icons8-pikachu-pokemon-96.png';
+    document.getElementById('id-figther-right').innerText = 'Pikachu';
 }
 
 // SELECT BACKGROUND
