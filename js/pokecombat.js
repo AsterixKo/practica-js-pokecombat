410c9b58fcc435542fbb5672b42d1aa1abc4b8c7
MSI\moneo <jvr.moneo@gmail.com>
2020-10-02 12:48:18 +0200

    Añadir imagenes de pokemons. Añadir funcionalidad al cambio de background. Añadir estilos.

---
+++ js/pokecombat.js
@@ -0,0 +1,16 @@
+
+
+
+// SELECT BACKGROUND
+function selectBackground1(){
+    document.body.style.backgroundImage = "url('img/escenario1.jpg')";
+}
+function selectBackground2(){
+    document.body.style.backgroundImage = "url('img/escenario2.png')";
+}
+function selectBackground3(){
+    document.body.style.backgroundImage = "url('img/escenario3.webp')";
+}
+function selectBackground4(){
+    document.body.style.backgroundImage = "url('img/escenario4.svg')";
+}
